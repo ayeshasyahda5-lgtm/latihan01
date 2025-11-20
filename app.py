3bbc8f8071c3826c9133fb9db1c352fb9e964e02
ayesha syahda ardiningrum <ayeshasyahda5@gmail.com>
2025-11-20 15:55:00 +0700

    menambahkan emot

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 pages = [
-    st.Page(page="pages/page1.py", title="Home", icon="🎀"),
+    st.Page(page="pages/page1.py", title="Home", icon="🌼"),
     st.Page(page="pages/page2.py", title="Visualisasi Data", icon="🌸"),
-    st.Page(page="pages/page3.py", title="Settings", icon="🌟"),
+    st.Page(page="pages/page3.py", title="Settings", icon="🌻"),
 ]
 
 pg = st.navigation(
